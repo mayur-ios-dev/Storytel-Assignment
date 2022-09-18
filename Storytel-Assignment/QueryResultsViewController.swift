e9455a374c5b99ba854c108eb06055a0ccfdfe65
mayur-ios-dev <mayur.deshmukh@voiapp.io>
2022-09-18 17:13:52 +0200

    Add QueryResultsViewController and SnapshotTests

---
+++ Storytel-Assignment/QueryResultsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  QueryResultsViewController.swift
+//  Storytel-Assignment
+//
+//  Created by Mayur Deshmukh on 2022-09-18.
+//
+
+import UIKit
+
+final class QueryResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
