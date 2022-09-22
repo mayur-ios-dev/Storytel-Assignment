894e4f48a5ea930bb7e3193f1f9484fcec25ce50
mayur-ios-dev <mayur.deshmukh@voiapp.io>
2022-09-23 00:31:14 +0200

    Implement ApiImageLoaderService

--- Storytel-Assignment/Network/ImageLoader/ApiImageLoaderService.swift
+++ Storytel-Assignment/Network/ImageLoader/ApiImageLoaderService.swift
@@ -12,3 +12,57 @@ protocol ApiImageLoaderServiceType {
     func image(withUrlString: String?) -> AnyPublisher<UIImage, Error>
 }
 
+final class ApiImageLoaderService {
+    private let session: URLSessionType
+    
+    init(session: URLSessionType = URLSession.shared) {
+        self.session = session
+    }
+}
+
+extension ApiImageLoaderService: ApiImageLoaderServiceType {
+    func image(withUrlString urlString: String?) -> AnyPublisher<UIImage, Error> {
+        guard let urlString = urlString else {
+            return .throwing(ApiImageLoaderServiceError.nilImageUrl)
+        }
+        guard let url = URL(string: urlString) else {
+            return .throwing(
+                URLError(
+                    .badURL,
+                    userInfo: [
+                        "failedImageUrl": urlString
+                    ]
+                )
+            )
+        }
+        let urlrequest = URLRequest(
+            url: url,
+            cachePolicy: .returnCacheDataElseLoad,
+            timeoutInterval: 120
+        )
+        
+        return session
+            .dataTaskPublisher(for: urlrequest)
+            .tryMap { data, _ in
+                guard let image = UIImage(data: data) else {
+                    throw ApiImageLoaderServiceError.couldNotBuildImageFromData(data)
+                }
+                return image
+            }.eraseToAnyPublisher()
+    }
+}
+
+enum ApiImageLoaderServiceError: Error {
+    case nilImageUrl
+    case couldNotBuildImageFromData(Data)
+}
+
+extension AnyPublisher where Output == UIImage, Failure == Error {
+    static func throwing(_ error: Error) -> Self {
+        Just(UIImage())
+            .tryMap { _ in
+                throw error
+            }
+            .eraseToAnyPublisher()
+    }
+}
