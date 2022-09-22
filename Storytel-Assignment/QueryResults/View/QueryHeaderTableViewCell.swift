86d437f8a50063dc0119218e898a7378241f9de1
mayur-ios-dev <mayur.deshmukh@voiapp.io>
2022-09-22 15:05:07 +0200

    Add QueryHeaderTableViewCell and SnapshotTests

---
+++ Storytel-Assignment/QueryResults/View/QueryHeaderTableViewCell.swift
@@ -0,0 +1,95 @@
+//
+//  QueryHeaderTableViewCell.swift
+//  Storytel-Assignment
+//
+//  Created by Mayur Deshmukh on 2022-09-21.
+//
+
+import UIKit
+
+class QueryHeaderTableViewCell: UITableViewCell {
+    
+    private var queryLabel: UILabel!
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupView()
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupView()
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        queryLabel.text = nil
+    }
+}
+
+private extension QueryHeaderTableViewCell {
+    func setupView() {
+        contentView.backgroundColor = .clear
+        
+        let (mainView, constraints) = makeMainView()
+        contentView.addSubview(mainView)
+        contentView.addConstraints(constraints)
+    }
+    
+    func makeMainView() -> (UIView, [NSLayoutConstraint]) {
+        queryLabel = UILabel()
+        queryLabel.translatesAutoresizingMaskIntoConstraints = false
+        queryLabel.font = .queryFontSize
+        queryLabel.textColor = .black
+        queryLabel.textAlignment = .center
+        queryLabel.numberOfLines = 0
+        
+        let constraints = [
+            queryLabel.topAnchor.constraint(
+                equalTo: contentView.topAnchor,
+                constant: .verticalPadding)
+            ,
+            contentView.bottomAnchor.constraint(
+                equalTo: queryLabel.bottomAnchor,
+                constant: .verticalPadding
+            ),
+            queryLabel.leadingAnchor.constraint(
+                equalTo: contentView.leadingAnchor,
+                constant: .horizontalPadding)
+            ,
+            contentView.trailingAnchor.constraint(
+                equalTo: queryLabel.trailingAnchor,
+                constant: .horizontalPadding
+            ),
+            contentView.heightAnchor.constraint(equalToConstant: .headerSize)
+        ]
+        
+        return (queryLabel, constraints)
+    }
+}
+
+// MARK: View layout constants
+
+private extension UIFont {
+    static let queryFontSize = UIFont.systemFont(ofSize: .bookTitleFontSize, weight: .regular)
+}
+
+private extension CGFloat {
+    static let bookTitleFontSize: CGFloat = 24
+    static let verticalPadding: CGFloat = 12
+    static let horizontalPadding: CGFloat = 12
+    static let headerSize: CGFloat = 160
+}
+
+// MARK: - Interface
+
+extension QueryHeaderTableViewCell {
+    func set(query: String) {
+        queryLabel.text = "Query: " + query
+    }
+}
---
+++ Storytel-AssignmentTests/QueryHeaderTableViewCellSnapshotTests.swift
@@ -0,0 +1,37 @@
+//
+//  QueryHeaderTableViewCellSnapshotTests.swift
+//  Storytel-AssignmentTests
+//
+//  Created by Mayur Deshmukh on 2022-09-22.
+//
+
+import XCTest
+@testable import Storytel_Assignment
+import SnapshotTesting
+
+final class QueryHeaderTableViewCellSnapshotTests: XCTestCase {
+    var view: QueryHeaderTableViewCell!
+    
+    override func setUpWithError() throws {
+        view = QueryHeaderTableViewCell(frame: .zero)
+    }
+
+    override func tearDownWithError() throws {
+        view = nil
+    }
+
+    func testLoadedState() {
+        assertSnapshot(matching: view.resizedContentViewForSnapshotTest, as: .image)
+    }
+    
+    func testShortText() {
+        view.set(query: "Short query")
+        assertSnapshot(matching: view.resizedContentViewForSnapshotTest, as: .image)
+    }
+    
+    func testLongText() {
+        view.set(query: "This is a very very long query")
+        assertSnapshot(matching: view.resizedContentViewForSnapshotTest, as: .image)
+    }
+
+}
