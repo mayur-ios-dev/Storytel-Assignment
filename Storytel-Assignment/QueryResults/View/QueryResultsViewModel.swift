42fa3b1698fe9d0f983b1c9f2094b7172e6a95d8
mayur-ios-dev <mayur.deshmukh@voiapp.io>
2022-09-23 01:48:24 +0200

    Add QueryResultsViewModel and tests

--- Storytel-Assignment/QueryResults/View/QueryResultsViewModel.swift
+++ Storytel-Assignment/QueryResults/View/QueryResultsViewModel.swift
@@ -19,7 +19,118 @@ protocol QueryResultsViewModelType {
 }
 
 final class QueryResultsViewModel {
+    private var queryMetadata: QueryMetadataModel
+    private var api: ApiQueryServiceType
+    private var imageLoader: ApiImageLoaderServiceType
     
+    private var output = PassthroughSubject<Output, Never>()
+    
+    private var queryResult: ApiQueryResult?
+    
+    private var subscriptions = Set<AnyCancellable>()
+    
+    init(queryMetadata: QueryMetadataModel,
+         api: ApiQueryServiceType = ApiQueryService(),
+         imageLoader: ApiImageLoaderServiceType = ApiImageLoaderService()) {
+        self.queryMetadata = queryMetadata
+        self.api = api
+        self.imageLoader = imageLoader
+    }
+}
+
+// MARK: - QueryResultsViewModelType
+extension QueryResultsViewModel: QueryResultsViewModelType {
+    var numberOfSections: Int { 3 }
+    
+    func numberOfRows(inSection section: Int) -> Int {
+        switch section {
+        case 0: return 1
+        case 1: return queryResult?.items.count ?? 0
+        case 2: return queryResult.hasMoreItemsToLoad ? 1 : 0
+        default : return 0
+        }
+    }
+    
+    var queryString: String { queryMetadata.query }
+    
+    func queryResult(at index: Int) throws -> QueryResultCellModel {
+        guard let items = queryResult?.items else {
+            throw QueryResultsViewModelError.queryResultsRequestedWhenNotLoaded
+        }
+        guard index < items.count else {
+            throw QueryResultsViewModelError.indexOutOfBound
+        }
+        return queryResultCellModel(from: items[index])
+    }
+    
+    func map(_ input: AnyPublisher<Input, Never>) -> AnyPublisher<Output, Never> {
+        input.sink { [weak self] receivedInput in
+            switch receivedInput {
+            case .willDisplayCell(at: let indexPath):
+                guard indexPath.section == 2 else { return }
+                self?.getResults()
+            }
+        }.store(in: &subscriptions)
+        return output
+            .receive(on: RunLoop.main)
+            .eraseToAnyPublisher()
+    }
+}
+
+private extension QueryResultsViewModel {
+    func getResults() {
+        guard queryResult.hasMoreItemsToLoad else { return }
+        do {
+            try api.search(
+                queryMetadata.query,
+                filter: queryMetadata.filter,
+                store: queryMetadata.store,
+                nextPageToken: queryResult?.nextPageToken
+            ).sink(receiveCompletion: { _ in
+                // log error
+            }, receiveValue: { [weak self] newQueryResult in
+                self?.queryResult = self?.queryResult.appending(newQueryResult)
+                if newQueryResult.items.count > 0 {
+                    self?.output.send(.dataFetched)
+                }
+            }).store(in: &subscriptions)
+        } catch {
+            // log error
+        }
+    }
+    
+    func queryResultCellModel(from item: ApiQueryResult.Item) -> QueryResultCellModel {
+        QueryResultCellModel(
+            image: imageLoader.image(
+                withUrlString: item.formats.first?.cover.url
+            )
+            .receive(on: RunLoop.main)
+            .eraseToAnyPublisher(),
+            bookTitle: item.title,
+            authors: item.authors.map{ $0.name }.joined(separator: ", "),
+            narrators: item.narrators.map{ $0.name }.joined(separator: ", ")
+        )
+    }
+}
+
+// MARK: - ApiQueryResult Helpers
+private extension Optional where Wrapped == ApiQueryResult {
+    func appending(_ apiQueryResult: ApiQueryResult) -> Self {
+        guard let self = self else { return apiQueryResult }
+        
+        return ApiQueryResult(
+            query: apiQueryResult.query,
+            filter: apiQueryResult.filter,
+            nextPageToken: apiQueryResult.nextPageToken,
+            totalCount: apiQueryResult.totalCount,
+            items: self.items + apiQueryResult.items
+        )
+    }
+    
+    var hasMoreItemsToLoad: Bool {
+        guard let self = self else { return true }
+        return self.items.count < self.totalCount
+    }
 }
 
 // MARK: - Input and Output
@@ -31,4 +142,10 @@ extension QueryResultsViewModel {
     enum Output {
         case dataFetched
     }
+}
+
+// MARK: - QueryResultsViewModelError
+enum QueryResultsViewModelError: Error {
+    case indexOutOfBound
+    case queryResultsRequestedWhenNotLoaded
 }
---
+++ Storytel-AssignmentTests/QueryResultsViewModelTests.swift
@@ -0,0 +1,189 @@
+//
+//  QueryResultsViewModelTests.swift
+//  Storytel-AssignmentTests
+//
+//  Created by Mayur Deshmukh on 2022-09-23.
+//
+
+import XCTest
+@testable import Storytel_Assignment
+import Combine
+
+final class QueryResultsViewModelTests: XCTestCase {
+    private var viewModel: QueryResultsViewModel!
+    private var api: MockApiService!
+    private var imageLoader: MockImageLoader!
+    
+    private var subscriptions = Set<AnyCancellable>()
+    
+    override func setUpWithError() throws {
+        api = MockApiService()
+        viewModel = QueryResultsViewModel(
+            queryMetadata: .init(
+                query: "Harry",
+                filter: .narrators,
+                store: "STHP-SE"
+            ),
+            api: api
+        )
+    }
+    
+    override func tearDownWithError() throws {
+        viewModel = nil
+        api = nil
+        imageLoader = nil
+    }
+    
+    func testOnDisplayingLoadingSectionMakesApiCall() throws {
+        let input = PassthroughSubject<QueryResultsViewModel.Input, Never>()
+        let outputExpectation = expectation(description: "Output Expectation")
+        viewModel
+            .map(input.eraseToAnyPublisher())
+            .sink { output in
+                XCTAssertEqual(output, .dataFetched)
+                outputExpectation.fulfill()
+            }
+            .store(in: &subscriptions)
+        
+        input.send(.willDisplayCell(at: .init(row: 0, section: 2)))
+        
+        XCTAssertEqual(api.query, "Harry")
+        XCTAssertEqual(api.store, "STHP-SE")
+        XCTAssertEqual(api.filter, .narrators)
+        XCTAssertEqual(api.nextPageToken, nil)
+        
+        api.output.send(.mock(nextPageToken: "123", totalCount: 4))
+        
+        XCTAssertEqual(viewModel.numberOfSections, 3)
+        XCTAssertEqual(viewModel.numberOfRows(inSection: 0), 1)
+        XCTAssertEqual(viewModel.numberOfRows(inSection: 1), 2)
+        XCTAssertEqual(viewModel.numberOfRows(inSection: 2), 1)
+        
+        let queryResult = try viewModel.queryResult(at: 0)
+        
+        XCTAssertEqual(queryResult.bookTitle, "test Title1")
+        
+        XCTAssertEqual(viewModel.numberOfRows(inSection: 2), 1)
+        
+        waitForExpectations(timeout: 0.1)
+    }
+    
+    func testAppendingItemsAndUpdatedNextPageToken() {
+        let input = PassthroughSubject<QueryResultsViewModel.Input, Never>()
+        viewModel
+            .map(input.eraseToAnyPublisher())
+            .sink { output in
+                XCTAssertEqual(output, .dataFetched)
+            }
+            .store(in: &subscriptions)
+        
+        // First loading
+        input.send(.willDisplayCell(at: .init(row: 0, section: 2)))
+        
+        XCTAssertEqual(api.query, "Harry")
+        XCTAssertEqual(api.store, "STHP-SE")
+        XCTAssertEqual(api.filter, .narrators)
+        XCTAssertEqual(api.nextPageToken, nil)
+        
+        api.output.send(.mock(nextPageToken: "123", totalCount: 4))
+        api.output.send(completion: .finished)
+        
+        XCTAssertEqual(viewModel.numberOfSections, 3)
+        XCTAssertEqual(viewModel.numberOfRows(inSection: 0), 1)
+        XCTAssertEqual(viewModel.numberOfRows(inSection: 1), 2)
+        XCTAssertEqual(viewModel.numberOfRows(inSection: 2), 1)
+        
+        api.output = PassthroughSubject<ApiQueryResult, Error>()
+        input.send(.willDisplayCell(at: .init(row: 0, section: 2)))
+        
+        XCTAssertEqual(api.query, "Harry")
+        XCTAssertEqual(api.store, "STHP-SE")
+        XCTAssertEqual(api.filter, .narrators)
+        XCTAssertEqual(api.nextPageToken, "123")
+        
+        
+        api.output.send(.mock(nextPageToken: "456", totalCount: 4))
+        
+        XCTAssertEqual(viewModel.numberOfSections, 3)
+        XCTAssertEqual(viewModel.numberOfRows(inSection: 0), 1)
+        XCTAssertEqual(viewModel.numberOfRows(inSection: 1), 4)
+        XCTAssertEqual(viewModel.numberOfRows(inSection: 2), 0, "Loading cell shown even though all items were loaded")
+    }
+    
+    func testNotDisplayingLoadingSectionDoesNotMakeApiCall() {
+        let input = PassthroughSubject<QueryResultsViewModel.Input, Never>()
+        viewModel
+            .map(input.eraseToAnyPublisher())
+            .sink { output in
+                XCTAssertEqual(output, .dataFetched)
+            }
+            .store(in: &subscriptions)
+        
+        input.send(.willDisplayCell(at: .init(row: 0, section: 0)))
+        input.send(.willDisplayCell(at: .init(row: 0, section: 1)))
+        input.send(.willDisplayCell(at: .init(row: 1, section: 1)))
+        
+        XCTAssertEqual(api.query, nil)
+        XCTAssertEqual(api.store, nil)
+        XCTAssertEqual(api.filter, nil)
+        XCTAssertEqual(api.nextPageToken, nil)
+        
+        api.output.send(.mock(nextPageToken: "123", totalCount: 4))
+    }
+}
+
+private class MockApiService: ApiQueryServiceType {
+    var output = PassthroughSubject<ApiQueryResult, Error>()
+    var query: String?
+    var filter: QueryMetadataModel.SearchFilter?
+    var store: String?
+    var nextPageToken: String?
+    
+    func search(_ query: String,
+                filter: QueryMetadataModel.SearchFilter?,
+                store: String,
+                nextPageToken: String?) throws -> AnyPublisher<ApiQueryResult, Error> {
+        self.query = query
+        self.filter = filter
+        self.store = store
+        self.nextPageToken = nextPageToken
+        return output.eraseToAnyPublisher()
+    }
+}
+
+private class MockImageLoader: ApiImageLoaderServiceType {
+    var urlString: String?
+    private var output = PassthroughSubject<UIImage, Error>()
+    
+    func image(withUrlString urlString: String?) -> AnyPublisher<UIImage, Error> {
+        self.urlString = urlString
+        return output.eraseToAnyPublisher()
+    }
+}
+
+private extension ApiQueryResult {
+    static func mock(nextPageToken: String, totalCount: Int) -> ApiQueryResult {
+        .init(
+            query: "Harry",
+            filter: "narrator",
+            nextPageToken: nextPageToken,
+            totalCount: totalCount,
+            items: [
+                .init(
+                    id: "testId1",
+                    title: "test Title1",
+                    authors: [],
+                    narrators: [],
+                    formats: []
+                ),
+                .init(
+                    id: "testId2",
+                    title: "test Title2",
+                    authors: [],
+                    narrators: [],
+                    formats: []
+                )
+            ]
+        )
+    }
+}
