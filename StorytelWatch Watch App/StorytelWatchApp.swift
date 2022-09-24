8902408972b0e0d85cfb926c5b3a04d94ec956c0
mayur-ios-dev <mayur.deshmukh@voiapp.io>
2022-09-24 14:58:52 +0200

    Add watch app target

---
+++ StorytelWatch Watch App/StorytelWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  StorytelWatchApp.swift
+//  StorytelWatch Watch App
+//
+//  Created by Mayur Deshmukh on 2022-09-24.
+//
+
+import SwiftUI
+
+@main
+struct StorytelWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
